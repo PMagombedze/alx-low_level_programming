348097563f04f19d15a8e58172cbb01123db0f82
PMagombedze <percymagoras@outlook.com>
2023-02-17 23:20:38 +0000

    putchar

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char ch = '0';
+char ch2 = 'a';
+for (ch = '0'; ch <= '9'; ch = ch + 1)
+{
+putchar(ch);
+}
+for (ch2 = 'a'; ch2 <= 'f'; ch2 = ch2 + 1)
+{
+putchar(ch2);
+}
+return (0);
+}
