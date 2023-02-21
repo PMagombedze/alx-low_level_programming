caf8a18109be48cc25141f57df7939c470a48631
PMagombedze <percymagoras@outlook.com>
2023-02-21 19:29:12 +0000

    23

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,21 @@
 #include <stdio.h>
 
+void print_alphabet();
+
 void putChar()
 {
 char name[] = "_putchar";
 printf("%s\n", name);
+}
+
+
+void print_alphabet()
+{
+char ch1 = 'a';
+for (ch1 = 'a'; ch1 <= 'z'; ch1 += 1)
+{
+putchar(ch1);
+}
+putchar('\n');
 }
 
