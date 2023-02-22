d6d049c29c0b92459e9ee60be2d1aab5e480ad1c
PMagombedze <percymagoras@outlook.com>
2023-02-22 18:12:59 +0000

    10x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,12 +8,12 @@ void print_alphabet_x10(void)
 {
 int i = 1;
 char ch1 = 'a';
+for (i = 1; i <= 10; i += 1)
+{
 for (ch1 = 'a'; ch1 <= 'z'; ch1 += 1)
 {
 _putchar(ch1);
 }
-for (i = 1; i <= 10; i += 1)
-{
-_putchar(ch1);
 }
+return (0);
 }
