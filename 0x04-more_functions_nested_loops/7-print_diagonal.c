9260769775e37be8e58c3586cd23bea11e346642
PMagombedze <percymagoras@outlook.com>
2023-02-23 23:59:42 +0000

    77

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,16 +9,20 @@
 void print_diagonal(int n)
 {
 int i;
-
+int j;
 for (i = 1; i <= n; i += 1)
 {
 if (n <= 0)
 {
 _putchar('\n');
 }
-else
+for(j = 0; j < n; j++)
 {
+if(j == i)
 _putchar('\\');
+else
+_putchar(' ');
 }
+_putchar('\n');
 }
 }
