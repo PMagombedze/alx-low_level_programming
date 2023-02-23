bbc6fdf74c0a7ca14f95911d9a3135a82d4278b3
PMagombedze <percymagoras@outlook.com>
2023-02-23 22:26:51 +0000

    3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main"
+
+/**
+ * print_numbers - print digits 0 to 9
+ * Return: numbers 0 to 9
+ */
+
+void print_numbers(void)
+{
+char i = '0';
+for (i = '0'; i <= '9'; i += 1)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
