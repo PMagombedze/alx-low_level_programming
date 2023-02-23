24989273589ec44d0b5e2f22c6d1411b6285fa5b
PMagombedze <percymagoras@outlook.com>
2023-02-23 21:44:37 +0000

    0.3

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,8 +2,8 @@
 
 /**
  *_isupper - check if character is uppercase
- *@c : variable to be checked
- *Return : 1 if c is uppercase, 0 otherwise
+ *@c: variable to be checked
+ *Return: 1 if c is uppercase, 0 otherwise
  */
 
 int _isupper(int c)
