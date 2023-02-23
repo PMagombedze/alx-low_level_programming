6d1cab60dd5e46426bb8f2793abe359c4881c714
PMagombedze <percymagoras@outlook.com>
2023-02-23 23:19:15 +0000

    5.4

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -4,13 +4,6 @@
  * more_numbers - print 0 to 14 10 times
  */
 
-void print_long(long value) {
-if(value != 0) {
-print_long(value / 10);
-_putchar((value % 10) + '0');
-}
-}
-
 void more_numbers(void)
 {
 int j = 0;
@@ -20,7 +13,7 @@ for (j = 0; j <= 10; j += 1)
 {
 for (i = 0; i < 15; i += 1)
 {
-print_long(i);
+_putchar(i + '0');
 }
 _putchar('\n');
 }
