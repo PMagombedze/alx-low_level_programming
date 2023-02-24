19e65f29e4c6dafe4a21bf7d822093bb35733f3a
PMagombedze <percymagoras@outlook.com>
2023-02-24 00:16:49 +0000

    8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,13 +9,20 @@
 void print_square(int size)
 {
 int i;
+int j;
 for (i = 1; i <= size; i++)
 {
-for (i = 1; i <= size; i++)
+for (j = 1; j <= size; j++)
+{
+if (size > 0)
 {
 _putchar('#');
 }
+else
+{
 _putchar('\n');
 }
+}
 _putchar('\n');
+}
 }
