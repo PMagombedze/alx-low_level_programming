0066000db2543095c6f6490d835b270a6ea4247d
PMagombedze <percymagoras@outlook.com>
2023-02-24 00:06:20 +0000

    8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_square - print square from n
+ *@n: n dimension square
+ *Return: square
+ */
+
+void print_square(int size)
+{
+int i;
+for (i = 1; i <= size; i++)
+{
+for (i = 1; i <= size; i++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+_putchar('\n');
+}
