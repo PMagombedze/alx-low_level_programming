99bedada23386895ee81527dc4f91f002ee92a78
PMagombedze <percymagoras@outlook.com>
2023-02-27 22:21:30 +0000

    6.1

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,10 +7,15 @@
 
 void puts2(char *str)
 {
-int i;
-for (i = 0; str[i] != 0; i = i + 2)
+int i = 0;
+
+while (str[i] != 0)
+{
+if (i % 2 == 0)
 {
 _putchar(str[i]);
+}
+i++;
 }
 _putchar('\n');
 }
