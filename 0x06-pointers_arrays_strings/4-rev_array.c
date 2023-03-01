dcf0f09ec761562a9dbfc456fdd0f37d84b55918
PMagombedze <percymagoras@outlook.com>
2023-03-01 21:23:58 +0000

    4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * reverse_array - funct to reverse int array
+ * @a: array pointer
+ * @n: number of array elements
+ */
+
+void reverse_array(int *a, int n)
+{
+int i = 0;
+int temp;
+n = n - 1;
+while (i <= n)
+{
+temp = a[i];
+a[i++] = a[n];
+a[n--] = temp.;
+}
+}
