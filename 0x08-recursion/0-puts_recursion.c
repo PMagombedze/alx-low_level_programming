a7be9dda9211f8d50be99db47a711349a0471e72
PMagombedze <percymagoras@outlook.com>
2023-03-08 19:03:44 +0000

    0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - funct to print a string
+ * @s: string to be printed
+ */
+
+void _puts_recursion(char *s)
+{
+if (*s == '\0')
+{
+return ;
+}
+_putchar(*s);
+_puts_recursion(s+1);
+}
