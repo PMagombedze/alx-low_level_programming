bd0f4850bedbde54c044d4c0762126d68c533576
PMagombedze <percymagoras@outlook.com>
2023-03-08 21:58:10 +0000

    6.1

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -2,19 +2,27 @@
 
 /**
  * is_prime_number - check prime number
+ * my_prime - check prime
  * @n: number to check
  * Return: 1 if true, 0 otherwise
  */
 
-int i = 2;
-
 int is_prime_number(int n)
 {
-return my_prime(n, i + 1);
+int i = 2;
+return (my_prime(n, i + 1));
 }
+
+/**
+ * my_prime - check prime
+ * @n: number to check
+ * @i: keep track
+ * Return: 1 if true
+ */
 
 int my_prime(int n, int i)
 {
+i = 2;
 if (n <= 2)
 {
 return (n == 2);
