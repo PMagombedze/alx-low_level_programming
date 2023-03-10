2ebf6d0aec89fe024a38af25fc2113a8eb4f9899
PMagombedze <percymagoras@outlook.com>
2023-03-10 22:14:24 +0000

    4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * main - main function
+ * @argc: argument count
+ * @argv:.argument vector
+ * Return: void
+ */
+
+int main(int argc, char *argv[])
+{
+if (argc != 1)
+{
+printf("0\n");
+}
+return (0);
+}
