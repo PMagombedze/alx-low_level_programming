0a740c41b125f3978b3bc740cdb5755795ec5ba2
PMagombedze <percymagoras@outlook.com>
2023-03-10 21:37:39 +0000

    1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: void
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+printf("%d\n", argc);
+return (0);
+}
