308e8db5d30d39b01f5b2d05ec1855c39e82bc25
PMagombedze <percymagoras@outlook.com>
2023-03-11 02:34:35 +0800

    final

---
+++ 0x09-static_libraries/4-strpbrk.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strpbrk - calculates the absolute value of an integer
+ * @s: s
+ * @accept: accept
+ * Return: value
+ */
+
+
+char *_strpbrk(char *s, char *accept)
+{
+return (0);
+}
