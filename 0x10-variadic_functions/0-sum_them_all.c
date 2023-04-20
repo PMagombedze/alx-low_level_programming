a1cb55f7b6f4172066c737450f058fb4537d7f42
PMagombedze <percymagoras@outlook.com>
2023-04-20 23:08:57 +0200

    update sum

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,20 +9,17 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
+va_list args;
 unsigned int Sum = 0, i = 0;
 if (n == 0)
 {
 return (0);
 }
-else
-{
-va_list args;
 va_start(args, n);
 for (i = 0; i < n; i++)
 {
 Sum += va_arg(args, int);
 }
 va_end(args);
-}
 return (Sum);
 }
