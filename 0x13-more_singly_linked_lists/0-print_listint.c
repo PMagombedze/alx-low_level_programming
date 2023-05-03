fe3230518ca6cea9320aa5d60b1fa1af560d1de6
PMagombedze <percymagoras@outlook.com>
2023-05-03 13:49:53 +0200

    updated print_listint

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,7 +3,7 @@
 /**
  * print_listint - prints all the elements of a listint_t list
  * @h: header pointer
- * 
+ *
  * Return: number of nodes
  */
 size_t print_listint(const listint_t *h)
@@ -13,7 +13,7 @@ size_t count = 0;
 while (temp != NULL)
 {
 printf("%d\n", temp->n);
-count+=1;
+count += 1;
 temp = temp->next;
 }
 return (count);
---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * listint_len - returns the number of elements in a linked listint_t list
+ * @h: header pointer
+ * 
+ * Return: number of elements in list
+ */
+size_t listint_len(const listint_t *h)
+{
+
+}
