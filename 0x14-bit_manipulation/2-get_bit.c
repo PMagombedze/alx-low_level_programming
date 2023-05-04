e4454b3b922920c852fe109fa7fa22880ba9f9cf
PMagombedze <percymagoras@outlook.com>
2023-05-04 23:20:56 +0200

    get_bit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * get_bit - convery binary to integer
+ * @n: integer
+ * @index: index
+ *
+ * Return: value of the bit at index, -1 otherwise
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+unsigned int temp;
+temp = n >> index;
+return (temp & 1);
+}
