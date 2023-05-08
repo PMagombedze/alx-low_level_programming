d98a283080e7230ea78bd4a983244bf24e9a80d5
PMagombedze <percymagoras@outlook.com>
2023-05-08 23:12:53 +0200

    readme

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,21 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <fcntl.h>
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif /* MAIN_H */
