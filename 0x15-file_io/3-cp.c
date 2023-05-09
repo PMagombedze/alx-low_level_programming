19378e471ed0223f38bf84ac4d2d2aaa8fa57b93
PMagombedze <percymagoras@outlook.com>
2023-05-09 03:56:45 +0200

    update copy content

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -9,44 +9,44 @@
  */
 int main(int argc, char const *argv[])
 {
-    unsigned int ch = 1024;
-    char buffer[1024];
-    int f_from, f_to;
-    int err;
-    if (argc != 3)
-    {
-    dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
-    exit(97);
-    }
-    f_to = open(argv[2], O_CREAT | O_WRONLY | O_APPEND, 0664);
-    f_from = open(argv[1], O_RDONLY);
-    if (f_from == -1)
-    {
-    dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-    exit(98);
-    }
-    if (f_to == -1)
-    {
-    dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-    exit(99);
-    }
-    for (; ch == 1024; )
-    {
-    ch = read(f_from, buffer, 1024);
-    write(f_to, buffer, ch);
-    }
-    err = close(f_from);
-    if (err == -1)
-    {
-    dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f_from);
-    exit(100);
-    }
-    err = close(f_to);
-    if (err == -1)
-    {
-    dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f_to);
-    exit(100);
-    }
-    return (0);
+unsigned int ch = 1024;
+char buffer[1024];
+int f_from, f_to;
+int err;
+if (argc != 3)
+{
+dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
+exit(97);
+}
+f_to = open(argv[2], O_CREAT | O_WRONLY | O_APPEND, 0664);
+f_from = open(argv[1], O_RDONLY);
+if (f_from == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+exit(98);
+}
+if (f_to == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+exit(99);
+}
+for (; ch == 1024; )
+{
+ch = read(f_from, buffer, 1024);
+write(f_to, buffer, ch);
+}
+err = close(f_from);
+if (err == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f_from);
+exit(100);
+}
+err = close(f_to);
+if (err == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f_to);
+exit(100);
+}
+return (0);
 }
 
