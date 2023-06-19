ebe85f7e8ef88464cd5f154c3d4b975dc0610ea0
PMagombedze <percymagoras@outlook.com>
2023-06-19 22:04:52 +0200

    operations

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,65 @@
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int div(int a, int b);
+int mod(int a, int b);
+
+/**
+* add - adds two integers
+* @a: first integer
+* @b: second integer
+* Return: the sum
+*/
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+* sub - subtracts two integers
+* @a: first integer
+* @b: second integer
+* Return: subtract
+*/
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+* mul - multiplies two integers
+* @a: first integer
+* @b: second integer
+* Return: the multiple
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+* div - divideds two integers
+* @a: first integer
+* @b: second integer
+* Return: divisiom
+*/
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+* mod - finds the modulus of two integers
+* @a: first integer
+* @b: second integer
+* Return: the modulus
+*/
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
